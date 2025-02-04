5268c48ed458dbd1febdc63f2afbf738b37e2174
caiseL <cristophertraperoo@gmail.com>
2025-02-03 22:50:17 -0700

    Show more information about the BST structure on our main script

--- 1/main.py
+++ 1/main.py
@@ -3,13 +3,18 @@ from bst import BinarySearchTree
 
 def main():
     bst = BinarySearchTree[int]()
-    bst.insert(1)
-    bst.insert(3)
-    bst.insert(4)
-    bst.insert(2)
-    bst.insert(6)
 
+    # Insert 10 numbers in the tree
+    [bst.insert(x) for x in range(10, 0, -1)]
+
+    print("Números en el arbol: ")
     bst.print()
+
+    num_to_search = 0
+    print(f"Buscando el número {num_to_search} en el árbol")
+    print(
+        f"El número {num_to_search} {'sí' if bst.search(num_to_search) else 'no'} está en el árbol"
+    )
 
 
 if __name__ == "__main__":
